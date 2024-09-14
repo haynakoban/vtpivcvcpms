b1ab40dad94bfd9765c8d1c7c26fcf58c47272af
Bryan Cortez <bancortez2@gmail.com>
2024-09-14 19:02:54 +0800

    video conf

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { ThemeProvider } from "@/components/common/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import AppRoutes from "@/routes";
 
 function App() {
@@ -9,6 +11,23 @@ function App() {
       <Router>
         <AppRoutes />
         <Toaster />
+        <ToastContainer
+          toastClassName={() =>
+            "relative flex py-4 px-3 rounded overflow-hidden cursor-pointer bg-white shadow-lg"
+          }
+          bodyClassName={() => "text-black text-base font-normal"}
+          position="bottom-left"
+          autoClose={4000}
+          hideProgressBar={true}
+          newestOnTop={false}
+          closeButton={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
       </Router>
     </ThemeProvider>
   );
