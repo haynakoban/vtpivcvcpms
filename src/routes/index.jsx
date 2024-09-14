fe9a802b4618db495339e81b9e62b0204fbfcb8b
Bryan Cortez <bancortez2@gmail.com>
2024-09-14 20:12:46 +0800

    routing for video conf

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 
 import MainLayout from "@/layout";
 
+// public routes
 import Home from "@/view";
 import AboutUs from "@/view/public/about-us";
 import Login from "@/view/public/login";
 import Register from "@/view/public/register";
 import Forgot from "@/view/public/forgot";
 import CatchAllPage from "@/view/public/404";
+
+// private routes
+import Video from "@/view/private/video";
 
 export default function AppRoutes() {
   return (
@@ -19,6 +23,7 @@ export default function AppRoutes() {
         <Route path="register" element={<Register />} />
         <Route path="forgot" element={<Forgot />} />
       </Route>
+      <Route path="video" element={<Video />} />
       <Route path="*" element={<CatchAllPage />} />
     </Routes>
   );
--- src/view/index.jsx
+++ src/view/index.jsx
@@ -1,104 +1,20 @@
-// import HeroSection from "@/components/sections/hero-section";
-// import ServicesSection from "@/components/sections/services-section";
-// import PetsFamilySection from "@/components/sections/pets-family-section";
-// import TestimonialsSection from "@/components/sections/testimonials-section";
-import { MeetingAppProvider } from "@/MeetingAppContextDef";
-import { MeetingContainer } from "@/meeting/MeetingContainer";
-import { LeaveScreen } from "@/components/video/screens/LeaveScreen";
-import { JoiningScreen } from "@/components/video/screens/JoiningScreen";
-import { useEffect, useState } from "react";
-import { MeetingProvider } from "@videosdk.live/react-sdk";
+import HeroSection from "@/components/sections/hero-section";
+import ServicesSection from "@/components/sections/services-section";
+import PetsFamilySection from "@/components/sections/pets-family-section";
+import TestimonialsSection from "@/components/sections/testimonials-section";
 
 export default function Home() {
-  const [token, setToken] = useState("");
-  const [meetingId, setMeetingId] = useState("");
-  const [participantName, setParticipantName] = useState("");
-  const [micOn, setMicOn] = useState(false);
-  const [webcamOn, setWebcamOn] = useState(false);
-  const [customAudioStream, setCustomAudioStream] = useState(null);
-  const [customVideoStream, setCustomVideoStream] = useState(null);
-  const [isMeetingStarted, setMeetingStarted] = useState(false);
-  const [isMeetingLeft, setIsMeetingLeft] = useState(false);
-
-  const isMobile = window.matchMedia(
-    "only screen and (max-width: 768px)"
-  ).matches;
-
-  useEffect(() => {
-    if (isMobile) {
-      window.onbeforeunload = () => {
-        return "Are you sure you want to exit?";
-      };
-    }
-  }, [isMobile]);
-
   return (
-    <>
-      <MeetingAppProvider>
-        {isMeetingStarted ? (
-          <MeetingProvider
-            config={{
-              meetingId,
-              micEnabled: micOn,
-              webcamEnabled: webcamOn,
-              name: participantName ? participantName : "TestUser",
-              multiStream: true,
-              customCameraVideoTrack: customVideoStream,
-              customMicrophoneAudioTrack: customAudioStream,
-            }}
-            token={token}
-            reinitialiseMeetingOnConfigChange={true}
-            joinWithoutUserInteraction={true}
-          >
-            <MeetingContainer
-              onMeetingLeave={() => {
-                setToken("");
-                setMeetingId("");
-                setParticipantName("");
-                setWebcamOn(false);
-                setMicOn(false);
-                setMeetingStarted(false);
-              }}
-              setIsMeetingLeft={setIsMeetingLeft}
-            />
-          </MeetingProvider>
-        ) : isMeetingLeft ? (
-          <LeaveScreen setIsMeetingLeft={setIsMeetingLeft} />
-        ) : (
-          <JoiningScreen
-            participantName={participantName}
-            setParticipantName={setParticipantName}
-            setMeetingId={setMeetingId}
-            setToken={setToken}
-            micOn={micOn}
-            setMicOn={setMicOn}
-            webcamOn={webcamOn}
-            setWebcamOn={setWebcamOn}
-            customAudioStream={customAudioStream}
-            setCustomAudioStream={setCustomAudioStream}
-            customVideoStream={customVideoStream}
-            setCustomVideoStream={setCustomVideoStream}
-            onClickStartMeeting={() => {
-              setMeetingStarted(true);
-            }}
-            startMeeting={isMeetingStarted}
-            setIsMeetingLeft={setIsMeetingLeft}
-          />
-        )}
-      </MeetingAppProvider>
-    </>
+    <main>
+      <HeroSection />
+
+      <div className="container mx-auto">
+        <ServicesSection />
+      </div>
+
+      <PetsFamilySection />
+
+      <TestimonialsSection />
+    </main>
   );
-  // return (
-  //   <main>
-  //     <HeroSection />
-
-  //     <div className="container mx-auto">
-  //       <ServicesSection />
-  //     </div>
-
-  //     <PetsFamilySection />
-
-  //     <TestimonialsSection />
-  //   </main>
-  // );
 }
